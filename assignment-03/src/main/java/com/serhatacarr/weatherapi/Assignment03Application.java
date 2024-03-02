67a0e0a239ffda457fd87511b726282e8308544a
Serhat Acar <sserhatacarr@gmail.com>
2024-03-02 21:03:44 +0300

    feat(assignment-03): initialize project

---
+++ assignment-03/src/main/java/com/serhatacarr/weatherapi/Assignment03Application.java
@@ -0,0 +1,13 @@
+package com.serhatacarr.weatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment03Application.class, args);
+    }
+
+}
---
+++ assignment-03/src/test/java/com/serhatacarr/weatherapi/Assignment03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhatacarr.weatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
